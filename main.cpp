b6d5bf4f901fb9dd2337f5043a5fffbaf44145c1
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2023-10-02 11:16:32 +0900

    01_01

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
+#include <iostream>
 #include <stdio.h>
+#include <typeinfo>
 
 template <typename Type>
 Type Min(Type a, Type b) {
 	return a > b ? b : a;
 }
 
-template <>
-char Min<char>(char a, char b) {
-	printf_s("•¶Žš—ñˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
-	return 0;
+template <typename Type>
+void printMin(Type a) {
+	std::cout << typeid(a).name() << ":" << a << std::endl;
 }
 
+template <>
+void printMin<char>(char a) {
+	printf_s("•¶Žš—ñˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
+}
+
+
 int main() {
-	printf_s("int(1,2):%d\n", Min<int>(1, 2));
-	printf_s("float(1.0f,2.0f):%f\n", Min<float>(1.0f, 2.0f));
-	printf_s("double(1.0f,2.0f):%f\n", Min<double>(static_cast<double>(1.0f), static_cast<double>(2.0f)));
-	printf_s("char(a,b):%f\n", Min<char>('a', 'b'));
+	printMin(Min<int>(1, 2));
+	printMin(Min<float>(1.0f, 2.0f));
+	printMin(Min<double>(static_cast<double>(1.0f), static_cast<double>(2.0f)));
+	printMin(Min<char>('a', 'b'));
 	return 0;
 }
