e4adcb12468e9bb6ab6ae1f54ef5a4058021f428
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2023-10-23 11:36:13 +0900

    02_04

--- main.cpp
+++ main.cpp
@@ -32,7 +32,17 @@ int main() {
 	const size_t kShape = 2;
 	IShape* shape[kShape];
 	shape[0] = new Circle(10.0f);
-	shape[0] = new Circle(10.0f);
+	shape[1] = new RectAngle(10.0f,20.0f);
+
+	for (size_t i = 0; i < kShape; i++) {
+		shape[i]->Size();
+		shape[i]->Draw();
+	}
+
+	for (size_t i = 0; i < kShape; i++) {
+		delete shape[i];
+	}
+
 	return 0;
 }
 
