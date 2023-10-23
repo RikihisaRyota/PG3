271f1411084397de1e5e98ac6c382fb7cabcab15
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2023-10-23 10:44:24 +0900

    02_03修正

--- main.cpp
+++ main.cpp
@@ -1,78 +1,70 @@
 #include <stdio.h>
 #include <vector>
 
-struct Vector2 {
-	float x;
-	float y;
-};
-
-class Enemy {
+class Fruit {
 public:
-	virtual void Attack() = 0;
-	virtual void Move() = 0;
+	virtual void Name();
 protected:
-	Vector2 position_;
+	const char* name_;
 };
 
-class Apple : public Enemy {
+class Apple : public Fruit {
 public:
-	void Attack() override;
-	void Move() override;
+	Apple(const char* name);
+	void Name() override;
 };
-void Apple::Attack() {
-	printf("AppleAttack!\n");
-}
-void Apple::Move() {
-	position_.x += 1.0f;
-	printf("ApplePosition\nx:%.1f,y:%.1f\n",position_.x,position_.y);
-}
 
-class Orange :public Enemy {
+class Orange :public Fruit {
 public:
-	void Attack() override;
-	void Move() override;
+	Orange(const char* name);
+	void Name() override;
 };
-void Orange::Attack() {
-	printf("OrangeAttack!\n");
-}
-void Orange::Move() {
-	position_.x += 5.0f;
-	printf("OrangePosition\nx:%.1f,y:%.1f\n", position_.x, position_.y);
-}
 
 
 int main() {
-	const size_t kEnemySize = 3;
-	std::vector<Enemy*> enemies;
+	const size_t kFruitSize = 3;
+	std::vector<Fruit*> fruits;
 
 	// 生成
-	for (size_t i = 0; i < kEnemySize; i++) {
+	for (size_t i = 0; i < kFruitSize; i++) {
 		if (i < 1) {
-			Apple* apple=new Apple();
-			enemies.emplace_back(apple);
+			Apple* apple=new Apple("Apple");
+			fruits.emplace_back(apple);
 		}
 		else {
-			Orange* orange = new Orange();
-			enemies.emplace_back(orange);
+			Orange* orange = new Orange("Orange");
+			fruits.emplace_back(orange);
 		}
 	}
 
-	// 移動
-	printf("EnemyMove\n");
-	for (auto& enemy : enemies) {
-		enemy->Move();
-	}
-	printf("EnemyAttack\n");
-	// 攻撃
-	for (auto& enemy : enemies) {
-		enemy->Attack();
+	for (auto& fruit : fruits) {
+		fruit->Name();
 	}
 
 	// 破棄
-	for (size_t i = 0; i < enemies.size();i++) {
-		delete enemies.at(i);
+	for (size_t i = 0; i < kFruitSize;i++) {
+		delete fruits.at(i);
 	}
 	return 0;
 }
 
+void Fruit::Name() {
+	printf("Name:%s\n", name_);
+}
 
+Apple::Apple(const char* name) {
+	name_ = name;
+}
+
+void Apple::Name() {
+	printf("Name:%s\n", name_);
+}
+
+
+Orange::Orange(const char* name) {
+	name_ = name;
+}
+
+void Orange::Name() {
+	printf("Name:%s\n", name_);
+}
