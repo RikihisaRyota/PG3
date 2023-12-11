f8f1e19a5998a5c9290d6e6dc7382f7a3e07c9f5
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2023-12-11 11:15:48 +0900

    05_01

--- main.cpp
+++ main.cpp
@@ -1,6 +1,62 @@
 #include <stdio.h>
+#include <iostream>
 #include <Windows.h>
 
+#include <list>
+
 int main() {
+	std::list<const char*> stationName = {
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho"
+	};
+	std::cout << "1970”N‚ÌŽRŽèü‰wˆê——" << std::endl;
+	for (auto& name : stationName) {
+		std::cout << name << std::endl;
+	}
+	std::cout << "\n" << std::endl;
+
+	auto findStationNameIterator = std::find(stationName.begin(), stationName.end(), "Nippori");
+	findStationNameIterator++;
+	stationName.insert(findStationNameIterator, "Nishinippori");
+
+	std::cout << "2019”N‚ÌŽRŽèü‰wˆê——" << std::endl;
+	for (auto& name : stationName) {
+		std::cout << name << std::endl;
+	}
+	std::cout << "\n" << std::endl;
+
+	findStationNameIterator = std::find(stationName.begin(), stationName.end(), "Shinagawa");
+	findStationNameIterator++;
+	findStationNameIterator = stationName.insert(findStationNameIterator, "TakanawaGateway");
+	std::cout << "2022”N‚ÌŽRŽèü‰wˆê——" << std::endl;
+	for (auto& name : stationName) {
+		std::cout << name << std::endl;
+	}
 	return 0;
 }
