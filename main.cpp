47636fb346856649e882d215440ee19f5f38fe32
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2024-02-05 10:38:33 +0900

    07_03_確認課題

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#include <iostream>
 
+#include <chrono>
 #include <string>
 
 int main() {
 	std::string str(1000000, 'a');
+	std::chrono::system_clock::time_point waitTime = std::chrono::system_clock::now();
+	
 	std::string copy = str;
+	std::chrono::microseconds elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - waitTime);
+	std::cout << elapsedTime.count() << std::endl;
+
+	waitTime = std::chrono::system_clock::now();
 	std::string move = std::move(str);
+	elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - waitTime);
+	std::cout << elapsedTime.count() << std::endl;
 	return 0;
 }
